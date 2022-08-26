35ba943d115b628d5a10fbd87fd69ce51ac6f3e4
Odun Freeman <bornduringwar@gmail.com>
2022-08-26 15:24:54 -0400

    wrapped entire app with themeprovider

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,10 +1,21 @@
 import '../styles/globals.css';
 import { UserProvider } from '../context/user';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#f25633e2',
+      main: '#f84018',
+    },
+  },
+});
 function MyApp({ Component, pageProps }) {
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
+    <ThemeProvider theme={theme}>
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
+    </ThemeProvider>
   );
 }
 export default MyApp;
