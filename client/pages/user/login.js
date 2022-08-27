ba3e000d0b46bafb4e4a3692f9589d441ec6b0ec
Odun Freeman <bornduringwar@gmail.com>
2022-08-26 20:14:04 -0400

    added circular progress while waiting for server to respond to login attempt

--- client/pages/user/login.js
+++ client/pages/user/login.js
@@ -7,7 +7,10 @@ import {
   CardActions,
   CardContent,
   TextField,
+  CircularProgress,
+  Box,
 } from '@mui/material';
+import { useState } from 'react';
 import { useUserContext } from '../../context/user';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 const theme = createTheme({
@@ -24,24 +27,23 @@ function Login() {
   axios.defaults.withCredentials = true;
 
   const [user, setUser] = useUserContext();
-
+  const [waiting, setWaiting] = useState(false);
   const loginAuto = async e => {
     e.preventDefault();
-
+    setWaiting(true);
     try {
       const username = document.getElementById('username').value;
       const password = document.getElementById('password').value;
-      const form = document.querySelector('form');
 
       const userData = { username, password };
 
       const res = await axios.post(`${SERVER_URL}/user/login`, userData, {
         withCredentials: true,
       });
-      console.log(res.data + ' - login auto form');
-      if (res.data.user) {
+
+      if (res.data && res.data.user) {
         setUser(res.data.user);
-        console.log('redirect');
+        setWaiting(false);
         Router.push({
           pathname: '/user/[id]',
           query: { id: res.data.user._id },
@@ -54,13 +56,15 @@ function Login() {
     }
   };
   const login = async status => {
+    setWaiting(true);
     const loginUrl = `${SERVER_URL}/user/login`;
 
     if (status === 'admin') {
       const userData = { username: 'jayshonk', password: 'aptiv' };
       try {
         const res = await axios.post(loginUrl, userData);
-        if (res.data.user) {
+        if (res.data && res.data.user) {
+          setWaiting(false);
           setUser(res.data.user);
           Router.push({
             pathname: '/user/[id]',
@@ -77,7 +81,8 @@ function Login() {
       try {
         const res = await axios.post(loginUrl, userData);
 
-        if (res.data.user) {
+        if (res.data && res.data.user) {
+          setWaiting(false);
           setUser(res.data.user);
 
           Router.push({
@@ -121,17 +126,32 @@ function Login() {
                 />
               </CardContent>
               <CardActions className="flex flex-col items-center gap-3">
-                <Button type="submit" variant="outlined" fullWidth>
-                  login
-                </Button>
-                <div className="flex gap-3">
-                  <Button variant="outlined" onClick={() => login('default')}>
-                    <p className="login-link">login as guest</p>
+                {!waiting && (
+                  <Button type="submit" variant="outlined" fullWidth>
+                    login
                   </Button>
-                  <Button variant="outlined" onClick={() => login('admin')}>
-                    <p className="login-link"> login as ADMIN</p>
-                  </Button>
-                </div>
+                )}
+                {!waiting && (
+                  <div className="flex gap-3">
+                    <Button variant="outlined" onClick={() => login('default')}>
+                      <p className="login-link">login as guest</p>
+                    </Button>
+                    <Button variant="outlined" onClick={() => login('admin')}>
+                      <p className="login-link"> login as ADMIN</p>
+                    </Button>
+                  </div>
+                )}
+                {waiting && (
+                  <Box
+                    sx={{
+                      display: 'flex',
+
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <CircularProgress />
+                  </Box>
+                )}
               </CardActions>
             </form>
           </Card>
